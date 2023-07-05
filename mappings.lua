1f290793e896f7130fe29dec792b147e76640e2e
therealnv6 <devrawr@protonmail.com>
2023-07-05 13:13:29 +0200

    update bindings for rename

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,11 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<F2>"] = { '<cmd>lua require("renamer").rename()<cr>', "rename", opts = { noremap = true, silent = true } },
+    ["<leader>r"] = { '<cmd>lua require("renamer").rename()<cr>', "rename", opts = { noremap = true, silent = true } },
+  },
+  v = {
+    ["<leader>r"] = { '<cmd>lua require("renamer").rename()<cr>', "rename", opts = { noremap = true, silent = true } },
   },
 }
 
--- plugins.lua
+++ plugins.lua
@@ -100,24 +100,7 @@ local plugins = {
   {
     "filipdutescu/renamer.nvim",
     config = function()
-      vim.api.nvim_set_keymap(
-        "i",
-        "<F2>",
-        '<cmd>lua require("renamer").rename()<cr>',
-        { noremap = true, silent = true }
-      )
-      vim.api.nvim_set_keymap(
-        "n",
-        "<leader>rn",
-        '<cmd>lua require("renamer").rename()<cr>',
-        { noremap = true, silent = true }
-      )
-      vim.api.nvim_set_keymap(
-        "v",
-        "<leader>rn",
-        '<cmd>lua require("renamer").rename()<cr>',
-        { noremap = true, silent = true }
-      )
+      require("renamer").setup()
     end,
   },
 
